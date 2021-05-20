b94cd624a829e964469ccde35a1133ed958a4085
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2021-05-20 13:12:53 +0100

    New gem

---
+++ lib/har/net/http.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "http/version"
+
+module Har
+  module Net
+    module Http
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
